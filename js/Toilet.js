eacca6cedec62cf3aa3037fc6392ff72e94f5761
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-04-26 12:19:22 +0200

    loggen info

---
+++ js/Toilet.js
@@ -0,0 +1,7 @@
+export default class Toilet {
+	constructor(latitude, longitude, adress) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.adress = adress;
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 "use strict";
+import Toilet from "./Toilet.js";
 
 // de leaflet library is reeds geimporteerd, en beschikbaar als "L"
 // dit via de script en css tag in de index.html, en de "map" div die werd toegevoegd.
@@ -6,13 +7,13 @@
 const app = {
 	map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
 	init() {
-		var map = L.map("map").setView([50.8359935, 4.3355214], 13);
+		app.map = L.map("map").setView([50.8359935, 4.3355214], 13);
 
 		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		}).addTo(map);
+		}).addTo(app.map);
 
-		L.marker([50.8422045, 4.322677]).addTo(map).bindPopup("EHB.<br> kampus Kaai.").openPopup();
+		L.marker([50.8422045, 4.322677]).addTo(app.map).bindPopup("EHB.<br> kampus Kaai.").openPopup();
 
 		// initialise de kaart
 
@@ -23,7 +24,15 @@ const app = {
 	},
 	loadMarkers() {
 		// fetch de data van opendata.brussels.be
-		fetch("");
+		fetch("https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000")
+			.then((toilet) => toilet.json())
+			.then(function (data) {
+				console.log(data);
+				data.records.forEach(function (toiletInfo) {
+					const toilet = new Toilet(toiletInfo.fields.wgs84_long, toiletInfo.fields.wgs84_lat, toiletInfo.fields.rue);
+					console.log(toilet);
+				});
+			});
 		// als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
 	},
 	addMarker(lat, lon) {
@@ -32,3 +41,4 @@ const app = {
 };
 
 app.init();
+app.loadMarkers();
